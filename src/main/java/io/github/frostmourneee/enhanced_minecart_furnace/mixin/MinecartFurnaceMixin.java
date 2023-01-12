ced3892a0bfb47be2b9d58fb9ab5733a00b093c7
Frostmourneee <dolpad@yandex.ru>
2023-01-12 18:40:49 +0300

    Interaction mixin implemented and access transformer included

--- src/main/java/io/github/frostmourneee/enhanced_minecart_furnace/mixin/MinecartFurnaceMixin.java
+++ src/main/java/io/github/frostmourneee/enhanced_minecart_furnace/mixin/MinecartFurnaceMixin.java
@@ -1,19 +1,26 @@
 package io.github.frostmourneee.enhanced_minecart_furnace.mixin;
 
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.vehicle.MinecartFurnace;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static io.github.frostmourneee.enhanced_minecart_furnace.EnhancedMinecartFurnace.customPrint;
 
 @Mixin( MinecartFurnace.class )
 public abstract class MinecartFurnaceMixin {
 
-    @Inject(method = "tick", at = @At(value = "TAIL"))
-    private void onTick(CallbackInfo info) {
-        customPrint("haha");
+    @Inject(method = "interact", at = @At(value = "TAIL"))
+    private void onInteract(Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResult> cir) {
+        MinecartFurnace cart = (MinecartFurnace) (Object) this;
 
+        if (cart.fuel > 0 && player.isPassenger()) {
+            cart.xPush *= -1;
+            cart.zPush *= -1;
+        }
     }
 }
